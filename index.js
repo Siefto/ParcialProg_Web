359ef14a3d79c45476af91b7f997f4c92b4a674b
Siefto <anferpasa1234@gmail.com>
2022-01-20 03:19:39 -0500

    cambios falta tabla

---
+++ index.js
@@ -0,0 +1,15 @@
+function Guardar_contenido(){
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const telefono = document.getElementById('telefono').value;
+
+    localStorage.setItem("NOMBRE", nombre);
+    localStorage.setItem("EMAIL", email);
+    localStorage.setItem("TELEFONO", telefono);
+
+    document.location.href = "resultado.html";
+
+    return;
+
+
+}
---
+++ result.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', () =>{
+  
+    /*const params = (new URL(document.location)).searchParams;
+    const nombre = params.get('nombre');
+    const email = params.get('email');
+    const telefono = params.get('telefono');*/
+
+    const nombre = localStorage.getItem('NOMBRE');
+    const email = localStorage.getItem('EMAIL');
+    const telefono = localStorage.getItem('TELEFONO');
+
+    console.log(nombre);
+
+    document.getElementById('nombre_resultante').innerHTML = nombre;
+    document.getElementById('email_resultante').innerHTML = email;
+    document.getElementById('telefono_resultante').innerHTML = telefono;
+
+})
